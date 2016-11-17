ca612bce938f0b753abd3a2fcc3173b8d53443ca
davidalves1 <alves.david@outlook.com>
2016-11-17 00:27:12 -0200

    Incluindo rotas de teste e removendo o bower

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 (function() {
 	'use strict';
 
-	angular.module('lab', []);
+	angular.module('lab', ['ngRoute'])
+		.config(['$routeProvider', configRoutes]);
+
+	function configRoutes($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'src/js/views/default.html',
+				controller: ''
+			})
+			.when('/login', {
+				templateUrl: 'src/js/views/login.html',
+				controller: 'LoginController'
+			});
+
+	}
 })();
